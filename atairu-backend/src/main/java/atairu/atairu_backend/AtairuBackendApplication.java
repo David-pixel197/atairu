c456e2f9dd249b51f5a7d4cc445ca249f22c9646
David Ramos <184526704+David-pixel197@users.noreply.github.com>
2026-03-30 17:09:59 -0300

    Add initial backend structure with Maven configuration, Dockerfile, and application setup

---
+++ atairu-backend/src/main/java/atairu/atairu_backend/AtairuBackendApplication.java
@@ -0,0 +1,13 @@
+package atairu.atairu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtairuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtairuBackendApplication.class, args);
+	}
+
+}
---
+++ atairu-backend/src/test/java/atairu/atairu_backend/AtairuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package atairu.atairu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtairuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
